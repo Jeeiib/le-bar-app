0b14dc07124ad5ce898db454ee1148cb21a31630
Jean-Baptiste <renartjeanbaptiste@gmail.com>
2026-07-02 16:59:00 +0200

    chore: MCD aligne sur le schema final (export PNG) et suppression d'un repository inutilise

--- back/src/main/java/fr/lebarapp/api/repository/CocktailSizeRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.lebarapp.api.repository;
-
-import fr.lebarapp.api.domain.CocktailSize;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CocktailSizeRepository extends JpaRepository<CocktailSize, Long> {
-}
